57f320547a5b8e59efd953d6fb9c22e429334d71
James Saludario <jsaludario12@gmail.com>
2025-10-21 11:27:48 +0800

    Initial implementation for login test

---
+++ tests/login.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('Successful Login', async ({ browser }) => {
+
+  // Create a new incognito browser context
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  // Navigate to the login page
+  await page.goto('https://cebuflexi-web.vercel.app');
+
+  // Click the sign-in button
+  await page.locator('button:has-text("Sign In")').click(); 
+
+  // Locate the username/email and password input fields and fill them in
+  await page.locator('input#email').fill('bahogbilat@email.com');
+  await page.locator('input#password').fill('J@m3s121296');
+
+  // Locate the login button and click it
+  await page.locator('button[type="submit"]').click();
+
+  // Wait for navigation or a specific element to appear after login
+  await expect(page.locator('svg')).toBeVisible({ timeout: 5000 });
+  
+  // Close the browser context after the test
+  await context.close();
+
+});
